b350426dc52e051db07e3f576ea3a6304778576a
seu11ee <yslee2504@naver.com>
2025-04-17 22:39:42 +0900

    add TabView

--- TimerStamp/ContentView.swift
+++ TimerStamp/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            ForEach(0..<2) { index in
+                ZStack {
+                    Color(hue: Double(index) / 5.0, saturation: 0.6, brightness: 0.9)
+                    Text("Page \(index + 1)")
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundColor(.white)
+                }
+                .cornerRadius(15)
+                .padding()
+            }
         }
-        .padding()
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic)) // 페이지 스타일 적용
     }
 }
 
