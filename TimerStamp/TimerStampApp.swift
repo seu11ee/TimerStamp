23b9a7a0acb3466954ea03f67a3a62f03ea55ebf
seu11ee <yslee2504@naver.com>
2025-04-17 22:37:49 +0900

    Initial commit

---
+++ TimerStamp/TimerStampApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerStampApp.swift
+//  TimerStamp
+//
+//  Created by 이예슬 on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct TimerStampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimerStampTests/TimerStampTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimerStampTests.swift
+//  TimerStampTests
+//
+//  Created by 이예슬 on 4/17/25.
+//
+
+import Testing
+@testable import TimerStamp
+
+struct TimerStampTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
